907df870a957a0868f53e68b0a4ae7493af5b8d4
yeeway <yiwei.suuu@gmail.com>
2024-03-29 01:14:56 +0800

    style: set up shadcn/ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-
+      <Button>Click me!</Button>
     </main>
   );
 }
