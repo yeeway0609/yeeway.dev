c7496555128352fa3c3b3554743aacefac986060
yeeway <yiwei.suuu@gmail.com>
2024-04-06 18:11:37 +0800

    style: add ProjectCard component, add projects data and modify projects page styling

---
+++ src/data/projectsData.ts
@@ -0,0 +1,15 @@
+import { Project } from "@/lib/types";
+
+export const projectsData: Project[] = [
+  {
+    title: "2024 NCKU Bike Festival Website",
+    description: "Led the team to build a website within 3 months, resulted in over 10,000 users and 40,000 views.",
+    imageURL: "\/ncku_bike_festival.jpeg",
+    tags: ["Vue.js", "Tailwind CSS", "Line Login"],
+    websiteURL: "https://nckubikefestival.ncku.edu.tw",
+    githubURL: "https://github.com/gdsc-ncku/BikeFestival17th-Frontend",
+    githubTitle: "BikeFestival17th-Frontend",
+    blogURL: "sdvsv",
+    blogTitle: "sdfds"
+  }
+];
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -37,3 +37,15 @@ export type IssuesResponse = {
   nodes: Issue[];
   pageInfo: PageInfo;
 };
+
+export type Project = {
+  title: string;
+  description: string;
+  imageURL: string;
+  tags?: string[];
+  websiteURL?: string;
+  githubURL?: string;
+  githubTitle?: string;
+  blogURL?: string;
+  blogTitle?: string;
+}
