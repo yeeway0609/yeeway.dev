1944609cc09dd98c862e424499d9fc30a3e856b2
yeeway <yiwei.suuu@gmail.com>
2024-09-08 01:00:28 +0800

    refactor: import svg using @svgr/webpack in Footer

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,14 @@ const nextConfig = {
   images: {
     domains: ['avatars.githubusercontent.com', 'upload.wikimedia.org'],
   },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 
 export default nextConfig
