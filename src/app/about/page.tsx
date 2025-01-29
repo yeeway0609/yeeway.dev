671036c26308ba5c51958e4ea05b677dfed56794
yeeway <yiwei.suuu@gmail.com>
2025-01-30 00:02:02 +0800

    refactor: move SectionTitle function to the end of AboutPage component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,10 +2,6 @@ import { Metadata } from 'next'
 import { MyAvatar } from '@/components/MyAvatar'
 import { SpotifyPlayer } from '@/components/SpotifyPlayer'
 
-function SectionTitle({ children }: { children: string }) {
-  return <h2 className="mb-2 mt-4 text-2xl font-bold text-primary sm:my-4">{children}</h2>
-}
-
 export const metadata: Metadata = {
   title: 'About Me | yeeway.dev',
 }
@@ -54,4 +50,8 @@ export default function AboutPage() {
       </section>
     </div>
   )
+}
+
+function SectionTitle({ children }: { children: string }) {
+  return <h2 className="mb-2 mt-4 text-2xl font-bold text-primary sm:my-4">{children}</h2>
 }
