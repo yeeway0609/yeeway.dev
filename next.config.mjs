a6f94ed9e88efe4bdd4b5494e4877a2d7e29e06d
yeeway <yiwei.suuu@gmail.com>
2025-02-02 23:33:45 +0800

    chore: add remark plugins for MDX support and update dev script

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 import createMDX from '@next/mdx'
+import remarkFrontmatter from 'remark-frontmatter'
+import remarkMdxFrontmatter from 'remark-mdx-frontmatter'
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -13,6 +15,11 @@ const nextConfig = {
   },
 }
 
-const withMDX = createMDX({})
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [remarkFrontmatter, remarkMdxFrontmatter],
+    rehypePlugins: [],
+  },
+})
 
 export default withMDX(nextConfig)
