6d09ae81e5816191fe21827a8bf5d7b7da4771b2
yeeway <yiwei.suuu@gmail.com>
2025-02-28 18:00:04 +0800

    refactor: update ui button to v4

--- src/components/Header/ThemeSwitch.tsx
+++ src/components/Header/ThemeSwitch.tsx
@@ -5,12 +5,16 @@ import { useTheme } from 'next-themes'
 import { Button } from '@/components/ui/button'
 
 export function ThemeSwitch() {
-  const { setTheme } = useTheme()
+  const { theme, setTheme } = useTheme()
+
+  function toggleTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
 
   return (
-    <Button className="group relative cursor-pointer rounded-sm p-2" variant="ghost" size="icon">
-      <SunIcon className="absolute size-6 transition-colors group-hover:text-primary dark:hidden" onClick={() => setTheme('dark')} />
-      <MoonIcon className="absolute hidden size-6 transition-colors group-hover:text-primary dark:block" onClick={() => setTheme('light')} />
+    <Button className="group relative cursor-pointer rounded-sm p-2" variant="ghost" size="icon" onClick={toggleTheme}>
+      <SunIcon className="group-hover:text-primary absolute size-6 transition-colors dark:hidden" />
+      <MoonIcon className="group-hover:text-primary absolute hidden size-6 transition-colors dark:block" />
       <span className="sr-only">Toggle theme</span>
     </Button>
   )
