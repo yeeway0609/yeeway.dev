7ebb4093201c2cc9fe224ecf4247c9ec709d81bc
yeeway <yiwei.suuu@gmail.com>
2025-05-09 14:51:58 +0800

    chore: update shadcn component config

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,16 @@ function Card({ className, ...props }: React.ComponentProps<'div'>) {
 }
 
 function CardHeader({ className, ...props }: React.ComponentProps<'div'>) {
-  return <div data-slot="card-header" className={cn('flex flex-col gap-1.5 px-6', className)} {...props} />
+  return (
+    <div
+      data-slot="card-header"
+      className={cn(
+        '@container/card-header grid auto-rows-min grid-rows-[auto_auto] items-start gap-1.5 px-6 has-data-[slot=card-action]:grid-cols-[1fr_auto] [.border-b]:pb-6',
+        className
+      )}
+      {...props}
+    />
+  )
 }
 
 function CardTitle({ className, ...props }: React.ComponentProps<'div'>) {
@@ -20,12 +29,16 @@ function CardDescription({ className, ...props }: React.ComponentProps<'div'>) {
   return <div data-slot="card-description" className={cn('text-muted-foreground text-sm', className)} {...props} />
 }
 
+function CardAction({ className, ...props }: React.ComponentProps<'div'>) {
+  return <div data-slot="card-action" className={cn('col-start-2 row-span-2 row-start-1 self-start justify-self-end', className)} {...props} />
+}
+
 function CardContent({ className, ...props }: React.ComponentProps<'div'>) {
   return <div data-slot="card-content" className={cn('px-6', className)} {...props} />
 }
 
 function CardFooter({ className, ...props }: React.ComponentProps<'div'>) {
-  return <div data-slot="card-footer" className={cn('flex items-center px-6', className)} {...props} />
+  return <div data-slot="card-footer" className={cn('flex items-center px-6 [.border-t]:pt-6', className)} {...props} />
 }
 
-export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent }
+export { Card, CardHeader, CardFooter, CardTitle, CardAction, CardDescription, CardContent }
--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -5,7 +5,7 @@ import * as TooltipPrimitive from '@radix-ui/react-tooltip'
 
 import { cn } from '@/lib/utils'
 
-function TooltipProvider({ delayDuration, ...props }: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
+function TooltipProvider({ delayDuration = 0, ...props }: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
   return <TooltipPrimitive.Provider data-slot="tooltip-provider" delayDuration={delayDuration} {...props} />
 }
 
